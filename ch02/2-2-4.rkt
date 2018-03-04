e48e36ad754b1a403607e0a6f0f250098fafd0bf
masaponto <masaponto@gmail.com>
2018-03-04 23:00:45 +0900

    [ADD] 2-2

--- ch02/2-2-4.rkt
+++ ch02/2-2-4.rkt
@@ -1,9 +1,9 @@
 #lang racket
 
+(require (planet "sicp.ss" ("soegaard" "sicp.plt" 2 1)))
+
 ;; How to evaluate
 ;; C-c C-k
-
-(require (planet "sicp.ss" ("soegaard" "sicp.plt" 2 1)))
 
 ;; Reference: http://uents.hatenablog.com/entry/sicp/011-ch2.2.4.1.md
 (define wave
@@ -208,6 +208,8 @@
 (define (end-segment2 segment)
   (cdr segment))
 
+;; 2.49
+;; a
 (define (frame-painter tr br tl bl)
   (segments->painter
    (list (make-segment2 tr br)
@@ -215,11 +217,12 @@
          (make-segment2 bl tl)
          (make-segment2 tl tr))))
 
+;; b
 (define (x-painter tr br tl bl)
   (segments->painter
    (list (make-segment2 tr bl)
          (make-segment2 tl br))))
-
+#|
 (let ((tr (make-vect 0.5 0.5))
       (br (make-vect 0.5 0.1))
       (tl (make-vect 0.1 0.5))
@@ -231,7 +234,9 @@
       (tl (make-vect 0.1 0.5))
       (bl (make-vect 0.1 0.1)))
   (paint-hires (x-painter tr br tl bl)))
+|#
 
+;; c
 (define (mid-point a b)
   (make-vect2 (/ (+ (xcor-vect2 a) (xcor-vect2 b)) 2)
               (/ (+ (ycor-vect2 a) (ycor-vect2 b)) 2)))
@@ -247,11 +252,13 @@
            (make-segment2 c d)
            (make-segment2 d a)))))
 
+#|
 (let ((tr (make-vect 0.5 0.5))
       (br (make-vect 0.5 0.1))
       (tl (make-vect 0.1 0.5))
       (bl (make-vect 0.1 0.1)))
   (paint-hires (diamond-painter tr br tl bl)))
+|#
 
 (check-equal?
  (mid-point (make-vect2 0.0 0.0) (make-vect2 1.0 1.0))
@@ -260,3 +267,139 @@
 (check-equal?
  (mid-point (make-vect2 0.5 0.5) (make-vect2 0.5 0.1))
  (make-vect2 0.5 0.3))
+
+
+;; 2.50
+(define (transform-painter2 painter origin corner1 corner2)
+  (lambda (frame)
+    (let ((m (frame-coord-map frame)))
+      (let ((new-origin (m origin)))
+        (painter (make-frame
+                  new-origin
+                  (sub-vect2 (m corner1) new-origin)
+                  (sub-vect2 (m corner2) new-origin)))))))
+
+(define (flip-horiz2 painter)
+  (transform-painter2 painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+(define (rotate-180 painter)
+  (transform-painter2 painter
+                      (make-vect 1.0 1.0)
+                      (make-vect 0.0 1.0)
+                      (make-vect 1.0 0.0)))
+
+(define (rotate-270 painter)
+  (transform-painter2 painter
+                      (make-vect 0.0 1.0)
+                      (make-vect 0.0 0.0)
+                      (make-vect 1.0 1.0)))
+
+#|
+(paint-hires wave)
+(paint-hires (flip-horiz2 wave))
+(paint-hires (rotate-180 wave))
+(paint-hires (rotate-270 wave))
+|#
+
+#|
+(paint einstein)
+(paint (flip-horiz2 einstein))
+(paint (rotate-180 einstein))
+(paint (rotate-270 einstein))
+|#
+
+
+;; 2.51
+(define (below2 painter1 painter2)
+  (let ((split-point (make-vect 0.0 0.5)))
+    (let ((paint-bottom
+           (transform-painter2
+            painter1
+            (make-vect 0.0 0.0)
+            (make-vect 1.0 0.0)
+            split-point))
+          (paint-top
+           (transform-painter2
+            painter1
+            split-point
+            (make-vect 1.0 0.5)
+            (make-vect 0.0 1.0))
+           ))
+      (lambda (frame)
+        (paint-bottom frame)
+        (paint-top frame)))))
+
+(paint (below2 einstein einstein))
+
+(define (rotate-90 painter)
+  (transform-painter2 painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 0.0)))
+
+(define (below3 painter1 painter2)
+  (rotate-270 (beside (rotate-90 painter1) (rotate-90 painter2))))
+
+(paint (below3 einstein einstein))
+
+
+;; 2.52
+;; a
+
+(define wave-smile
+  (segments->painter
+   (list (make-segment (make-vect 0.2 0.0) (make-vect 0.4 0.4))
+         (make-segment (make-vect 0.4 0.4) (make-vect 0.3 0.5))
+         (make-segment (make-vect 0.3 0.5) (make-vect 0.1 0.3))
+         (make-segment (make-vect 0.1 0.3) (make-vect 0.0 0.6))
+         (make-segment (make-vect 0.0 0.8) (make-vect 0.1 0.5))
+         (make-segment (make-vect 0.1 0.5) (make-vect 0.3 0.6))
+         (make-segment (make-vect 0.3 0.6) (make-vect 0.4 0.6))
+         (make-segment (make-vect 0.4 0.6) (make-vect 0.3 0.8))
+         (make-segment (make-vect 0.3 0.8) (make-vect 0.4 1.0))
+         (make-segment (make-vect 0.6 1.0) (make-vect 0.7 0.8))
+         (make-segment (make-vect 0.7 0.8) (make-vect 0.6 0.6))
+         (make-segment (make-vect 0.6 0.6) (make-vect 0.8 0.6))
+         (make-segment (make-vect 0.8 0.6) (make-vect 1.0 0.4))
+         (make-segment (make-vect 1.0 0.2) (make-vect 0.6 0.4))
+         (make-segment (make-vect 0.6 0.4) (make-vect 0.8 0.0))
+         (make-segment (make-vect 0.7 0.0) (make-vect 0.5 0.3))
+         (make-segment (make-vect 0.5 0.3) (make-vect 0.3 0.0))
+
+         ;;eyes
+         (make-segment (make-vect 0.4 0.8) (make-vect 0.45 0.8))
+         (make-segment (make-vect 0.55 0.8) (make-vect 0.6 0.8))
+
+         ;; mouth
+         (make-segment (make-vect 0.43 0.7) (make-vect 0.58 0.7))
+         (make-segment (make-vect 0.4 0.72) (make-vect 0.43 0.7))
+         (make-segment (make-vect 0.58 0.7) (make-vect 0.6 0.72))
+         )))
+
+(paint-hires wave-smile)
+
+
+;; b
+(define (corner-split-2 painter n)
+  (if (= n 0)
+      painter
+      (let ((up (up-split painter (- n 1)))
+            (right (right-split painter (- n 1))))
+        (let ((top-left up)
+              (bottom-right right)
+              (corner (corner-split-2 painter (- n 1))))
+          (beside (below painter top-left)
+                  (below bottom-right corner))))))
+
+(paint-hires (corner-split-2 wave-smile 2))
+
+;; c
+(define (square-limit3 painter n)
+  (let ((combine4 (square-of-four flip-vert rotate180
+                                  identity flip-horiz)))
+    (combine4 (corner-split painter n))))
+
+(paint-hires (square-limit3 wave-smile 4))
