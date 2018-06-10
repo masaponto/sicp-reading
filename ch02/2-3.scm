3362a74b8b3c21afd11860473c1efa9616e330cc
masaponto <masaponto@gmail.com>
2018-06-10 20:26:14 +0900

    [Add] solve 2.58-a

--- ch02/2-3.scm
+++ ch02/2-3.scm
@@ -144,7 +144,6 @@
        (deriv '(* 3 (** x 2)) 'x))
 
 ;; 2.57
-
 (define (augend s)
   (let ((x (cddr s)))
     (cond ((or (pair? (car x))
@@ -209,3 +208,78 @@
        (deriv '(* x y (+ x 3)) 'x))
 
 ;; 2.58
+
+(define (sum? x) (and (pair? x) (eq? (cadr x) '+)))
+
+(define (addend s) (car s))
+
+(define (augend s) (caddr s))
+
+(define (product? x) (and (pair? x) (eq? (cadr x) '*)))
+
+(define (multiplier p) (car p))
+
+(define (multiplicand p) (caddr p))
+
+(define (exponentiation? x) (and (pair? x)
+                                 (eq? (cadr x) '**)))
+(define (base p) (car p))
+
+(define (exponent p) (caddr p))
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2))
+         (+ a1 a2))
+        (else (list a1 '+ a2))))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list m1 '* m2))))
+
+(define (make-exponentiation a1 a2)
+  (cond ((=number? a2 0) 1)
+        ((=number? a2 1) a1)
+        (else (list a1 '** a2))))
+
+(test-section "ex 2.58 - a")
+
+(test* "sum?" #t (sum? '(1 + 2)))
+(test* "sum?" #f (sum? '(+ 1 2)))
+
+(test* "addend" 1 (addend '(1 + 2)))
+
+(test* "augend" 2 (augend '(1 + 2)))
+
+(test* "make-sum" '(1 + x) (make-sum 1 'x))
+(test* "make-sum" 2 (make-sum 1 1))
+(test* "make-sum" 1 (make-sum 1 0))
+(test* "make-sum" '(x + y) (make-sum 'x 'y))
+
+(test* "product?" #f (product? '(* 1 2)))
+
+(test* "multiplier" 1 (multiplier '(1 * x)))
+
+(test* "multiplicand" 'x
+       (multiplicand '(1 * x)))
+
+(test* "make-sum" '(2 * x) (make-product 2 'x))
+(test* "make-sum" 1 (make-product 1 1))
+(test* "make-sum" 0 (make-product 1 0))
+(test* "make-sum" '(x * y) (make-product 'x 'y))
+
+(test* "deriv" 1
+       (deriv '(1 + x) 'x))
+
+(test* "deriv" 'y
+       (deriv '(x * y) 'x))
+
+(test* "deriv" '(3 + y)
+       (deriv '((3 * x) + (y * x)) 'x))
+
+(test* "deriv" 'x
+       (deriv '((3 * x) + (y * x)) 'y))
